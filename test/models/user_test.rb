840e994d634c6f09323ecef3cd56fb33d5373caf
Gaz Prometheus <gazprometheus@github.com>
2017-01-16 18:05:37 -0800

    up to gravatar

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,7 +44,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should reject invalid addresses" do
-  	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+  	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 	
 	invalid_addresses.each do |invalid_address|
 		@user.email = invalid_address
@@ -57,6 +57,13 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAmPlE.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should have minimum number of characters" do
